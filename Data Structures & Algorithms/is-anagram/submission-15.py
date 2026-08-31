f208d43380eb6df43d153ba272c34e3fbf06d521
Amir Ahmad <72308071+amirahmad001@users.noreply.github.com>
2026-08-31 08:46:37 +0530

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hm_s = {}
+        hm_t = {}
+        if len(s) != len(t):
+            return False
+        for i in s:
+            if i in hm_s:
+                hm_s[i] += 1
+            else:
+                 hm_s[i] = 1
+        for i in t:
+            if i in hm_t:
+                hm_t[i] += 1
+            else:
+                 hm_t[i] = 1
+        return hm_s == hm_t
+        
